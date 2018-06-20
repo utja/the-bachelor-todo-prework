199f6b7b3a912c20bde0fd18e5ee8c4a657dccad
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-20 04:38:38 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  #quicker to go into first level by entering season key specific to season we are looking for
+  data[season].each do |contestants|
+    if contestants["status"] == "Winner"
+      winner = contestants["name"].split
+      winner = winner[0]
+      return winner
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant_info|
+      if contestant_info["occupation"] == occupation
+        return contestant_info["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        i += 1
+      end
+    end
+  end
+  return i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        return contestant_info["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  tot_age = 0
+  num_of_contestants = 0
+  data[season].each do |contestants|
+    # need to change to int, all data stored as string
+    tot_age += (contestants["age"]).to_i
+    num_of_contestants += 1
+  end
+  return (tot_age.to_f/num_of_contestants.to_f).round
 end

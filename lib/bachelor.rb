523114054e11e397d309e33ecf3297448358790d
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-20 02:33:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |json_season, contestants|
     if json_season == season
       contestants.collect do |contestant|
-        if contestant
+        if contestant["status"] == "winner"
 end
 
 def get_contestant_name(data, occupation)
